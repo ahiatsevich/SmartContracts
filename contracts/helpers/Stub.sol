18bb7f66c7a5d7193d35244068cad2564383c3e9
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-04-25 20:33:24 +0300

    MINT-1439 Tests: add tests for TimeHolder's lock/unlock functionality

--- contracts/helpers/Stub.sol
+++ contracts/helpers/Stub.sol
@@ -8,4 +8,12 @@ pragma solidity ^0.4.11;
 // For testing purposes.
 contract Stub {
     function() public {}
+
+    function getHash(bytes32 _arg) public pure returns (bytes32) {
+        return keccak256(_arg);
+    }
+
+    function toBytes32(bytes32 _arg) public pure returns (bytes32) {
+        return _arg;
+    }
 }
