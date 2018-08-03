e3a799a6b5d4b605523a41c95e95ed5cfe7796e6
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-08-03 11:20:53 +0300

    MINT-1546 Update StorageManager to support authorized access
    It is needed to delegate giving access to Chronobank platform storage

--- contracts/core/storage/StorageManager.sol
+++ contracts/core/storage/StorageManager.sol
@@ -5,16 +5,60 @@
 
 pragma solidity ^0.4.11;
 
-import '../common/Object.sol';
-import '../event/MultiEventsHistoryAdapter.sol';
 
-contract StorageManager is MultiEventsHistoryAdapter, Object {
+import "../common/Object.sol";
+import "../event/MultiEventsHistoryAdapter.sol";
+
+
+contract StorageManagerEmitter is MultiEventsHistoryAdapter {
+
+    event AccessGiven(address indexed self, address indexed actor, bytes32 role);
+    event AccessBlocked(address indexed self, address indexed actor, bytes32 role);
+    event AuthorizationGranted(address indexed self, address indexed account);
+    event AuthorizationRevoked(address indexed self, address indexed account);
+    event Error(address indexed self, uint errorCode);
+
+    /// @dev Events history contract address
+    address private localEventsHistory;
+
+    function getEventsHistory() 
+    public 
+    view 
+    returns (address) 
+    {
+        address _eventsHistory = localEventsHistory;
+        return _eventsHistory != 0x0 ? _eventsHistory : address(this);
+    }
+
+    function emitAccessGiven(address _user, bytes32 _role) public {
+        emit AccessGiven(_self(), _user, _role);
+    }
+
+    function emitAccessBlocked(address _user, bytes32 _role) public {
+        emit AccessBlocked(_self(), _user, _role);
+    }
+
+    function emitAuthorizationGranted(address _account) public {
+        emit AuthorizationGranted(_self(), _account);
+    }
+
+    function emitAuthorizationRevoked(address _account) public {
+        emit AuthorizationRevoked(_self(), _account);
+    }
+
+    function _setEventsHistory(address _eventsHistory) internal {
+        localEventsHistory = _eventsHistory;
+    }
+
+    function _emitter() internal view returns (StorageManagerEmitter) {
+        return StorageManagerEmitter(getEventsHistory());
+    }
+}
+
+
+contract StorageManager is Object, StorageManagerEmitter {
 
     uint constant ERROR_STORAGE_INVALID_INVOCATION = 5000;
-
-    event AccessGiven(address indexed self, address actor, bytes32 role);
-    event AccessBlocked(address indexed self, address actor, bytes32 role);
-    event Error(address indexed self, uint errorCode);
 
     mapping (address => uint) public authorised;
     mapping (bytes32 => bool) public accessRights;
@@ -26,54 +70,88 @@ contract StorageManager is MultiEventsHistoryAdapter, Object {
         }
     }
 
-    function authorize(address _address) onlyAuthorized external returns (uint) {
+    function setupEventsHistory(address _eventsHistory) 
+    external 
+    onlyContractOwner 
+    returns (uint) 
+    {
+        require(_eventsHistory != 0x0);
+        _setEventsHistory(_eventsHistory);
+        return OK;
+    } 
+
+    function authorize(address _address) 
+    external 
+    onlyAuthorized 
+    returns (uint) 
+    {
         require(_address != 0x0);
         acl[_address] = true;
+
+        _emitter().emitAuthorizationGranted(_address);
         return OK;
     }
 
-    function revoke(address _address) onlyContractOwner external returns (uint) {
+    function revoke(address _address) 
+    external 
+    onlyContractOwner 
+    returns (uint) 
+    {
         require(acl[_address]);
         delete acl[_address];
+
+        _emitter().emitAuthorizationRevoked(_address);
         return OK;
     }
 
-    function giveAccess(address _actor, bytes32 _role) onlyAuthorized returns(uint) {
-        if (!accessRights[sha3(_actor, _role)]) {
-            accessRights[sha3(_actor, _role)] = true;
+    function giveAccess(address _actor, bytes32 _role) 
+    external 
+    onlyAuthorized 
+    returns (uint) 
+    {
+        if (!accessRights[_getKey(_actor, _role)]) {
+            accessRights[_getKey(_actor, _role)] = true;
             authorised[_actor] += 1;
-            emitAccessGiven(_actor, _role);
+            _emitter().emitAccessGiven(_actor, _role);
         }
 
         return OK;
     }
 
-    function blockAccess(address _actor, bytes32 _role) onlyAuthorized returns(uint) {
-        if (accessRights[sha3(_actor, _role)]) {
-            delete accessRights[sha3(_actor, _role)];
+    function blockAccess(address _actor, bytes32 _role) 
+    external 
+    onlyAuthorized 
+    returns (uint) 
+    {
+        if (accessRights[_getKey(_actor, _role)]) {
+            delete accessRights[_getKey(_actor, _role)];
             authorised[_actor] -= 1;
             if (authorised[_actor] == 0) {
                 delete authorised[_actor];
             }
-            emitAccessBlocked(_actor, _role);
+            _emitter().emitAccessBlocked(_actor, _role);
         }
 
         return OK;
     }
 
-    function isAllowed(address _actor, bytes32 _role) constant returns(bool) {
-        return accessRights[sha3(_actor, _role)] || (this == _actor);
+    function isAllowed(address _actor, bytes32 _role) 
+    public 
+    view 
+    returns (bool) 
+    {
+        return accessRights[_getKey(_actor, _role)] || (address(this) == _actor);
     }
 
-    function hasAccess(address _actor) constant returns(bool) {
-        return (authorised[_actor] > 0) || (this == _actor);
+    function hasAccess(address _actor) 
+    public 
+    view 
+    returns (bool) 
+    {
+        return (authorised[_actor] > 0) || (address(this) == _actor);
     }
 
-    function emitAccessGiven(address _user, bytes32 _role) {
-        AccessGiven(this, _user, _role);
-    }
-
-    function emitAccessBlocked(address _user, bytes32 _role) {
-        AccessBlocked(this, _user, _role);
+    function _getKey(address _actor, bytes32 _role) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_actor, _role));
     }
 }
